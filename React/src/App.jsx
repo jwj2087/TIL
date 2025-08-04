a6937d41b643bff6d5883d5a246f9d07338baba2
apple <apple@apples-MacBook-Air.local>
2025-08-04 17:35:21 +0900

    props 활용하기

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,7 +4,8 @@ function App(){ // 함수를 이용해 컴포넌트를 만들어 재사용할 �
   return ( // ()를 이용하면 여러줄에 걸쳐 작성이 가능하다.
     <div>
       App 컴포넌트!
-      <Dice /> 
+      {/* 컴포넌트에 속성(props)을 지정할 수 있다 */}
+      <Dice color='red'/>  
     </div>
   );
 }
--- React/src/Dice.jsx
+++ React/src/Dice.jsx
@@ -1,7 +1,19 @@
 import diceBlue01 from './assets/dice-blue-1.svg'; // 이미지 url을 사용할때는 import로 주소를 받아와서 사용
+import diceRed01 from './assets/dice-red-1.svg';
 
-function Dice(){
-  return <img src={diceBlue01} alt="주사위" /> // src에 직접적으로 주소값을 넣을 수 없다.
+function Dice(props){
+  console.log(props); // {color='blue'}
+
+  // props 값을 이용해서 렌더 내용을 바꿀 수 있다.
+  const diceImg = props.color === 'red' ? diceRed01 : diceBlue01; 
+  return (
+  <>
+    {/* // src에 직접적으로 주소값을 넣을 수 없다. */}
+    <img src={diceBlue01} alt="파란주사위" />  
+    {/* prop 값에 따라 이미지가 달라짐 */}
+    <img src={diceImg} alt='주사위' /> 
+  </>
+  );
 }
 
 export default Dice;
