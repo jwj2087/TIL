e5f965aa5718d8a39f029eed43d0501ca73012de
apple <apple@apples-MacBook-Air.local>
2025-08-06 11:37:36 +0900

    filter를 이용해서 listItem 삭제하기

--- MoviePedia/src/components/App.jsx
+++ MoviePedia/src/components/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import ReviewList from './ReviewList';
-import items from '../mock.json';
+import mockItems from '../mock.json';
 
 function App() {
   const [order, setOrder] = useState('createdAt'); // order의 초기값은 createAt
-  // 상태에 따라 정렬 시키기
-  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  const [items, setItems] = useState(mockItems); // item의 상태변화도 관리하기 위해 state로 생성
+  
+  const sortedItems = items.sort((a, b) => b[order] - a[order]); // 원하는 order 상태에 따라 정렬 시키기
 
   const handleNewestClick = () => setOrder('createdAt'); // 상태 = createdAt
   const handleBestClick = () => setOrder('rating'); // 상태 = rating
+
+  const handleDelete = (id) => { // filter를 이용해서 item을 삭제하는 함수
+    const nextItems = items.filter((item) => item.id !== id); // id값이랑 동일한 item을 제외하고 nextItems를 생성
+    setItems(nextItems); // items  상태 변경
+  };
 
   return (
     <div>
@@ -17,7 +23,7 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleBestClick}>베스트순</button>
       </div>
-      <ReviewList items={sortedItems} />
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
--- MoviePedia/src/components/ReviewList.jsx
+++ MoviePedia/src/components/ReviewList.jsx
@@ -1,11 +1,15 @@
 import './ReviewList.css';
 
-function formatDate(value) {
+function formatDate(value) { // 그냥 날짜 깔끔하게 보이게 하는 함수
   const date = new Date(value);
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function ReviewListItem({ item }) { // 각 리스트의 내용
+function ReviewListItem({ item, onDelete }) { // 각 리스트의 내용
+  const handleDeleteClick = () => { // 삭제버튼 클릭시
+    onDelete(item.id); // ReviewListItem -> ReviewList -> App으로 item.id 전달
+  };
+
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
@@ -14,18 +18,19 @@ function ReviewListItem({ item }) { // 각 리스트의 내용
         <p>{item.rating}</p>
         <p>{formatDate(item.createdAt)}</p>
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
-function ReviewList({ items }) { // 리스트
+function ReviewList({ items, onDelete }) { // 리스트
   return (
     <ul>
       {items.map((item) => { // map을 이용해서 렌더링
         return (
           <li> 
-            <ReviewListItem item={item} />
+            <ReviewListItem item={item} onDelete={onDelete} />
           </li>
         );
       })}
