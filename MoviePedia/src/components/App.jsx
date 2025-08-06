0bf20a6d34707c1be84a9dd49917d667bc548d9f
apple <apple@apples-MacBook-Air.local>
2025-08-06 11:09:04 +0900

    sort를 이용해서 list 정렬하기

--- MoviePedia/src/components/App.jsx
+++ MoviePedia/src/components/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import ReviewList from './ReviewList';
 import items from '../mock.json';
 
 function App() {
+  const [order, setOrder] = useState('createdAt'); // order의 초기값은 createAt
+  // 상태에 따라 정렬 시키기
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder('createdAt'); // 상태 = createdAt
+  const handleBestClick = () => setOrder('rating'); // 상태 = rating
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        {/* 각 버튼을 누르면 원하는 기준으로 list가 정렬된다 */}
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} />
     </div>
   );
 }
