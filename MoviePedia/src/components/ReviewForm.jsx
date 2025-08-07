bcd94c6632ea56725f4e293dc65b2a7628850058
apple <apple@apples-MacBook-Air.local>
2025-08-07 11:33:32 +0900

    입력폼을 하나의 상태로 관리하기

--- MoviePedia/src/components/ReviewForm.jsx
+++ MoviePedia/src/components/ReviewForm.jsx
@@ -2,39 +2,33 @@ import { useState } from 'react';
 import './ReviewForm.css';
 
 function ReviewForm() {
-  const [title, setTitle] = useState('');
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState('');
+  const [values, setValues] = useState({ // 상태 하나로 입력폼 관리
+    title: '',
+    rating: 0,
+    content: '',
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value);
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault(); // form submit 기본동작 막기
-    console.log({
-      title,
-      rating,
-      content,
-    });
+    console.log(values);
   };
 
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
       {/* onChange는 바닐라js의 onInput처럼 사용자가 값을 입력할때마다 발생하는 이벤트 */}
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input type="number" name="rating" value={values.rating} onChange={handleChange} />
+      <textarea name="content" value={values.content} onChange={handleChange} />
+      <button type="submit">확인</button>
     </form>
   );
 }
