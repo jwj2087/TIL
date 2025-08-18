19c07233d400f9b4ab67c144df9f4b0fb2175bba
apple <apple@apples-MacBook-Air.local>
2025-08-18 17:17:49 +0900

    queryString 처리하기

--- Todo-api/app.js
+++ Todo-api/app.js
@@ -1,11 +1,30 @@
 import express from 'express';
+import tasks from './data/mock.js';
 
 const app = express(); 
 
 // 첫번째 파라미터 : usl 경로
 // 두번째 파라미터 : 실행할 콜백함수 (들어온 리퀘스트 객체, 돌려줄 리스폰스 객체)
-app.get('/hello', (req, res) => {
-  res.send('hello express');
+app.get('/tasks', (req, res) => {
+  /**
+   * 쿼리 파라미터
+   * - sort: 'oldest'인 경우 오래된 태스크 기준, default는 새로운 태스트 기준
+   * - count: 태스크 개수
+   * 
+   * 쿼리 파라미터는 쿼리 객체에 전달 - 모두 문자열로 전달됨
+   */
+  const sort = req.query.sort;
+  const count = Number(req.query.count);
+
+  // 정렬 비교함수
+  const compareFn = sort === 'oldest' ? (a, b) => a.createdAt - b.createdAt : (a, b) => b.createdAt - a.createdAt;
+  let newTasks = tasks.sort(compareFn);
+  // count가 있으면 count만큼 task 잘라주기
+  if (count) {
+    newTasks = newTasks.slice(0, count);
+  }
+
+  res.send(newTasks); // JS 객체를 받으면 자동으로 JSON으로 변환해서 돌려준다. 
 });
 // 3000 : 포트번호
 app.listen(3000, () => console.log('Server start!'));
